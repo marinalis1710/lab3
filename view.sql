f0904fa181c5650ed57b32f49435c01bba99e3f9
marinalis1710 <62431328+marinalis1710@users.noreply.github.com>
2020-05-04 12:04:05 +0300

    Update view.sql

--- view.sql
+++ view.sql
@@ -1,5 +1,5 @@
-create view party as 
+create view cocktailbarhuman as 
 select humancocktail.human_name,
 humancocktail.cocktail_name from humancocktail
-join humanbar on humanbar.human_name=humancocktail.human_name
-join bar on bar.bar_name=humanbar.bar_name;
+inner join humanbar on humanbar.human_name=humancocktail.human_name
+inner join bar on bar.bar_name=humanbar.bar_name;
