71d2882430ca6e307dabcf82c11de0120f9e4f66
marinalis1710 <62431328+marinalis1710@users.noreply.github.com>
2020-05-04 15:39:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import chart_studio
+chart_studio.tools.set_credentials_file(username = 'mLisichenko', api_key='OALHe7ZRtB5XG1u7UQUK')
+import plotly.graph_objects as go
+import chart_studio.plotly as py
 import cx_Oracle
 
 username = 'Marina'
 password = 'xu40e5'
-
 database = 'localhost/xe'
-
 connection = cx_Oracle.connect(username, password, database)
 cursor = connection.cursor()
 
@@ -15,22 +17,34 @@ group by bar_name
 order by total_humans desc
 """
 cursor.execute(query1)
-print("Bar_name - Quantity_of_bartenders")
-for row in cursor:
 
-    print(row)
+bar_name = []
+quantity_bartenders = []
+for data in cursor.fetchall():
+    bar_name.append(data[0])
+    quantity_bartenders.append(data[1])
+
+bar = go.Bar(x=bar_name, y=quantity_bartenders)
+py.plot([bar], auto_open=True, filename='oracle-data')
 
 query2 = """
 select bar_name,
-concat(round(count(human_name)*100/(
-select count(*) from humanbar)),'%') as procent_of_total_bartenders from party
+count(human_name)*100/(
+select count(*) from humanbar) as procent_of_total_bartenders from party
 group by bar_name
 """
 cursor.execute(query2)
-print("\n Bar_name - %_quantity_of_bartenders")
-for row in cursor:
 
-    print(row)
+
+bar_name2 = []
+quantity_bartenders2 = []
+
+for row in cursor.fetchall():
+    bar_name2.append(row[0])
+    quantity_bartenders2.append(row[1])
+pie = go.Pie(labels=bar_name2, values=quantity_bartenders2)
+py.plot([pie], auto_open=True)
+
 
 query3 = """
 select count(cocktail_name) as quantity_of_cocktails,
@@ -40,7 +54,17 @@ group by city_name
 order by quantity_of_cocktails desc
 """
 cursor.execute(query3)
-print("\nQuantity_of_cocktails - Bar_location")
-for row in cursor:
 
-    print(row)
+quantity = []
+location = []
+
+for data in cursor.fetchall():
+    quantity.append(data[0])
+    location.append(data[1])
+
+scatter = go.Scatter(
+    x=location,
+    y=quantity
+)
+
+py.plot([scatter], auto_open=True)
