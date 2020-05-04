801c580441e9b50cefd0a618da2e645f0cc674c6
marinalis1710 <62431328+marinalis1710@users.noreply.github.com>
2020-05-04 16:27:01 +0300

    Update loop.sql

--- loop.sql
+++ loop.sql
@@ -1,19 +1,10 @@
 declare
-    city    bar.city_name%TYPE;
-    bar   bar.bar_name%TYPE;
+    city bar.city_name%TYPE;
+    bar  bar.bar_name%TYPE;
 begin
     city := 'City';
     bar := 'Bar';
-    FOR i IN 1..10 LOOP INSERT INTO bar (
-        city_name,
-        bar_name
-    ) VALUES (
-        TRIM(city)
-        || i,
-        TRIM(bar)
-        || i
-    );
-
+    FOR i IN 1..10 LOOP INSERT INTO bar (city_name,bar_name) 
+        VALUES (TRIM(city)|| i,TRIM(bar)|| i);
     end loop;
-
 end;
