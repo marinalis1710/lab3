29a5046644c2f01ff6f00d96083a57f3215fec07
marinalis1710 <62431328+marinalis1710@users.noreply.github.com>
2020-05-05 21:46:54 +0300

    Update export.py

--- export.py
+++ export.py
@@ -9,13 +9,16 @@ cursor = connection.cursor()
 
 tables = ['Bar', 'Human', 'Cocktail', 'HumanCocktail', 'HumanBar', 'City']
 
-try:
-    for i in tables:
+
+for i in tables:
+    try:
         with open(i + '.csv', 'w', newline='') as file:
             cursor.execute("SELECT * FROM " + i)
             database = cursor.fetchall()
             csv_writer=csv.writer(file, delimiter=',')
         for row in database:
             csv_writer.writerow(row)
+    except:
+        pass
 cursor.close()
 connection.close()
