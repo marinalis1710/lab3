f1e6d37695293e72764348dc8d49b853ee9fda44
marinalis1710 <62431328+marinalis1710@users.noreply.github.com>
2020-05-05 21:42:25 +0300

    Update kaggle_import.py

--- kaggle_import.py
+++ kaggle_import.py
@@ -26,33 +26,49 @@ with open(filename, newline='') as file:
             bar_name = row['Bar/Company']
             city_name = row['City']
 
-            if cocktail_name not in cocktail_list:
-                cocktail_list.append(cocktail_name)
-                query = """INSERT INTO Cocktail(cocktail_name) VALUES (:cocktail_name)"""
-                cursor.execute(query, cocktail_name=cocktail_name)
+            try:
+                if cocktail_name not in cocktail_list:
+                    cocktail_list.append(cocktail_name)
+                    query = """INSERT INTO Cocktail(cocktail_name) VALUES (:cocktail_name)"""
+                    cursor.execute(query, cocktail_name=cocktail_name)
+            except:
+                pass
+            try:
+                if human_name not in human_list:
+                    human_list.append(human_name)
+                    query = """INSERT INTO Human(human_name)  VALUES (:human_name)"""
+                    cursor.execute(query, human_name=human_name)
+            except:
+                pass
 
-            if human_name not in human_list:
-                human_list.append(human_name)
-                query = """INSERT INTO Human(human_name)  VALUES (:human_name)"""
-                cursor.execute(query, human_name=human_name)
+            try:
+                if city_name not in city_list:
+                    city_list.append(city_name)
+                    query = """INSERT INTO City(city_name) VALUES (:city_name)"""
+                    cursor.execute(query, city_name=city_name)
+            except:
+                pass
 
-            if city_name not in city_list:
-                city_list.append(city_name)
-                query = """INSERT INTO City(city_name) VALUES (:city_name)"""
-                cursor.execute(query, city_name=city_name)
+            try:
+                query = """INSERT INTO Bar(bar_name, city_name) VALUES (:bar_name, :city_name)"""
+                cursor.execute(query, bar_name=bar_name, city_name=city_name)
+            except:
+                pass
 
-            query = """INSERT INTO Bar(bar_name, city_name) VALUES (:bar_name, :city_name)"""
-            cursor.execute(query, bar_name=bar_name, city_name=city_name)
+            try:
+                query = """INSERT INTO HumanBar(human_name, bar_name) VALUES (:human_name, :bar_name)"""
+                cursor.execute(query, human_name=human_name, bar_name=bar_name)
+            except:
+                pass
 
-            query = """INSERT INTO HumanBar(human_name, bar_name) VALUES (:human_name, :bar_name)"""
-            cursor.execute(query, human_name=human_name, bar_name=bar_name)
-
-            query = """INSERT INTO HumanCocktail(human_name, cocktail_name) VALUES (:human_name, :cocktail_name)"""
-            cursor.execute(query, human_name=human_name, cocktail_name=cocktail_name)
-
+            try:
+                query = """INSERT INTO HumanCocktail(human_name, cocktail_name) VALUES (:human_name, :cocktail_name)"""
+                cursor.execute(query, human_name=human_name, cocktail_name=cocktail_name)
+            except:
+                pass
         except:
             pass
 
-connection.commit()
-cursor.close()
-connection.close()
+    connection.commit()
+    cursor.close()
+    connection.close()
